e7745964c98dd63382d0ab4561752094eec92bf3
YuriyYurev <iuriev-yuriy@mail.ru>
2024-10-18 07:40:55 +0700

    feat: block-statistic

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -7,6 +7,7 @@ import BlockHeroSlider from '@/components/blocks/block-hero-slider/block-hero-sl
 import BlockHeading from '@/components/blocks/block-heading/block-heading';
 import BlockProject from '@/components/blocks/block-project/block-project';
 import BlockMedia from '@/components/blocks/block-media/block-media';
+import BlockStatistic from '@/components/blocks/block-statistic/block-statistic';
 import CardProject from '@/components/blocks/card-project/card-project';
 import CardMedia from '@/components/blocks/card-media/card-media';
 import TitleMain from '@/components/ui/title-main/title-main';
@@ -23,6 +24,7 @@ const allComponents: Record<string, any & Component<HTMLElement>> = {
     'block-heading': BlockHeading,
     'block-project': BlockProject,
     'block-media': BlockMedia,
+    'block-statistic': BlockStatistic,
     'card-project': CardProject,
     'card-media': CardMedia,
     'title-main': TitleMain,
