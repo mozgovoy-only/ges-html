2cb686b46675b0ca712dbd061388a6735026ab85
YuriyYurev <iuriev-yuriy@mail.ru>
2024-10-23 14:36:06 +0700

    feat; sections-projects

---
+++ frontend/src/components/sections/section-projects/section-projects.ts
@@ -0,0 +1,15 @@
+import Component, { ComponentProps } from '@/base/component';
+
+type SectionProjectsOptions = {
+
+}
+
+export default class SectionProjects extends Component<HTMLElement, SectionProjectsOptions> {
+    constructor(element: ComponentProps<HTMLElement>) {
+        super(element);
+    }
+
+    destroy() {
+        // Destroy function
+    }
+}
