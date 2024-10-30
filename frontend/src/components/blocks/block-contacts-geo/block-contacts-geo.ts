61b89798fe585d43884c73d36e59f3a7ede1cf2c
YuriyYurev <iuriev-yuriy@mail.ru>
2024-10-30 10:58:12 +0700

    feat: block-contacts-geo

---
+++ frontend/src/components/blocks/block-contacts-geo/block-contacts-geo.ts
@@ -0,0 +1,15 @@
+import Component, { ComponentProps } from '@/base/component';
+
+type BlockContactsGeoOptions = {
+
+}
+
+export default class BlockContactsGeo extends Component<HTMLElement, BlockContactsGeoOptions> {
+    constructor(element: ComponentProps<HTMLElement>) {
+        super(element);
+    }
+
+    destroy() {
+        // Destroy function
+    }
+}
