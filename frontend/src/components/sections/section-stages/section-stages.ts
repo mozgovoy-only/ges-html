4ea0f278f7bad1dcfb37da988924284ed42e9d21
YuriyYurev <iuriev-yuriy@mail.ru>
2024-10-31 17:03:19 +0700

    feat: section-stages

---
+++ frontend/src/components/sections/section-stages/section-stages.ts
@@ -0,0 +1,15 @@
+import Component, { ComponentProps } from '@/base/component';
+
+type SectionStagesOptions = {
+
+}
+
+export default class SectionStages extends Component<HTMLElement, SectionStagesOptions> {
+    constructor(element: ComponentProps<HTMLElement>) {
+        super(element);
+    }
+
+    destroy() {
+        // Destroy function
+    }
+}
