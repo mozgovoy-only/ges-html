fecb94018a14f1c92b26d50e08968eebdd5ba0dc
YuriyYurev <iuriev-yuriy@mail.ru>
2024-11-07 21:33:41 +0700

    fix: dropdown

--- frontend/src/components/ui/dropdown/dropdown.ts
+++ frontend/src/components/ui/dropdown/dropdown.ts
@@ -1,17 +1,22 @@
 import Component, { ComponentProps } from '@/base/component';
-
+import Drop from 'tether-drop';
 type DropdownOptions = {
 
 }
 
 export default class Dropdown extends Component<HTMLElement, DropdownOptions> {
     button: HTMLButtonElement | null = null;
+    instance: Drop | null = null
     constructor(element: ComponentProps<HTMLElement>) {
         super(element);
         this.onButtonClick = this.onButtonClick.bind(this);
         this.init()
     }
     init() {
+        this.instance = new Drop({
+            target: this.nRoot as HTMLElement,
+            });
+
         this.button = this.getElement('button') as HTMLButtonElement;
 
         this.button?.addEventListener('click', this.onButtonClick)
@@ -20,6 +25,6 @@ export default class Dropdown extends Component<HTMLElement, DropdownOptions> {
     onButtonClick() {
     }
     destroy() {
-        // Destroy function
+        this.instance?.destroy();
     }
 }
