18a5ff612b2ecc2c1bca75a87472346e0e3b38ea
YuriyYurev <iuriev-yuriy@mail.ru>
2024-11-07 13:38:30 +0700

    fix: slider

--- frontend/src/components/sections/section-hero-main/section-hero-main.ts
+++ frontend/src/components/sections/section-hero-main/section-hero-main.ts
@@ -1,15 +1,26 @@
 import Component, { ComponentProps } from '@/base/component';
+import Swiper from 'swiper';
 
 type SectionHeroMainOptions = {
 
 }
 
 export default class SectionHeroMain extends Component<HTMLElement, SectionHeroMainOptions> {
+    slider: HTMLElement | null = null;
+    instance: Swiper | null = null;
     constructor(element: ComponentProps<HTMLElement>) {
         super(element);
+        this.init();
+    }
+
+    init() {
+        this.slider = this.getElement('slider') as HTMLDivElement;
+        this.instance = new Swiper(this.slider, {
+            slidesPerView: 1
+        })
     }
 
     destroy() {
-        // Destroy function
+        this.instance?.destroy();
     }
 }
