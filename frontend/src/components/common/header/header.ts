eaad373e778f9703effd1483a3461bca5eed9298
YuriyYurev <iuriev-yuriy@mail.ru>
2024-11-08 15:18:15 +0700

    fix: header.ts

--- frontend/src/components/common/header/header.ts
+++ frontend/src/components/common/header/header.ts
@@ -1,11 +1,26 @@
 import Component, { ComponentProps } from '@/base/component';
+import { scroll } from '../../../app/js/common';
 
 export default class Header extends Component<HTMLElement> {
+    isScrolling: boolean;
     constructor(element: ComponentProps<HTMLElement>) {
         super(element);
+        this.isScrolling = false;
+        this.onWindowScroll = this.onWindowScroll.bind(this);
+        this.init();
     }
+    init() {
+        window.addEventListener('scroll', this.onWindowScroll);
+    }
+    onWindowScroll() {
+        const { classList } = this.nRoot;
+        const { offsetHeight } = this.nRoot;
 
+        if (this.isScrolling === scroll.position.y > offsetHeight) return;
+        this.isScrolling = scroll.position.y > offsetHeight;
+        classList.toggle('is-scrolling', this.isScrolling);
+    }
     destroy() {
-        // Destroy functions
+        window.removeEventListener('scroll', this.onWindowScroll);
     };
 }
